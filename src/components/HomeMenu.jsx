1dd6522437d550ee8e589999692db4bad44928a5
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-06-03 14:54:43 +0200

    Add desktop version for menu section

--- src/components/HomeMenu.jsx
+++ src/components/HomeMenu.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import "../styles/components/HomeMenu.scss";
 import menuImage from "../assets/menu.png";
-import menuBg from "../assets/menu-bg.png";
+import bgMobile from "../assets/menu-bg.png";
+import bgDesktop from "../assets/menu-bg-desktop.png";
+import { useState } from "react";
+import { useEffect } from "react";
 const HomeMenu = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const windowWidthChangeHandelr = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", windowWidthChangeHandelr);
+    return () => {
+      window.removeEventListener("resize", windowWidthChangeHandelr);
+    };
+  }, []);
+
   return (
-    <section style={{ backgroundImage: `url(${menuBg})` }} className="menu">
-      <img src={menuImage} alt="menu" />
+    <section
+      style={{
+        backgroundImage: `url(${windowWidth >= 1024 ? bgDesktop : bgMobile})`,
+      }}
+      className="menu"
+    >
+      <div className="menu__container">
+        <img src={menuImage} alt="menu" />
+      </div>
     </section>
   );
 };
